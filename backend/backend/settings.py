e3b907ea5d081fae01692ffed8b4730fc79c776d
edijavier2699 <edison.canizares@tokunize.com>
2024-08-26 20:30:08 +0100

    sensitive data added just to check if it is working

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -160,13 +160,13 @@ CORS_ALLOWED_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 CORS_ALLOWED_HEADERS = ['Authorization', 'Content-Type']
 
 
-#EMAIL SETTINGS
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False
-# EMAIL_HOST_USER = 'eddy.javiieer@gmail.com'
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'eddy.javiieer@gmail.com'
+EMAIL_HOST_PASSWORD = 'pbxy pxrk gwbz hugi' 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
