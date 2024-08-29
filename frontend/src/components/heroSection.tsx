2626212ee32f0ee0f1d8f03b5927be4790b466eb
edijavier99 <edijavier10@gmail.com>
2024-08-29 09:04:57 +0100

    High Efficiency Video Coding for the section video, H265, and added an image for the lower power mode

--- frontend/src/components/heroSection.tsx
+++ frontend/src/components/heroSection.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import heroVideo from '../assets/video/hero-video-origial.mp4';
+import React, { useState } from 'react';
+import heroVideo from '../assets/video/heroVideo.mp4';
+import heroImage from '../assets/lowerModeImg.jpeg'; // La imagen de respaldo
 import '../styles/heroSection.css';
 import { NewsletterForm } from '@/forms/newsletterForm';
 
 export const HeroSection: React.FC = () => {
+  const [showVideo, setShowVideo] = useState(true);
+
+  const handleVideoError = () => {
+    setShowVideo(false); // Si hay un error, cambiamos a la imagen
+  };
+
+  const handleCanPlayThrough = () => {
+    setShowVideo(true); // Si el video puede reproducirse, lo mostramos
+  };
 
   return (
     <div className="relative my-5 mx-[20px] md:mx-[80px] rounded overflow-hidden heroVideoBackground">
-      {/* Video Background */}
-      <video
-        className="absolute inset-0 object-cover w-full h-full"
-        autoPlay
-        muted
-        loop
-        playsInline
-      >
-        <source src={heroVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      {showVideo ? (
+        <video
+          className="absolute inset-0 object-cover w-full h-full"
+          autoPlay
+          muted
+          loop
+          playsInline
+          onCanPlayThrough={handleCanPlayThrough} // Detecta si se puede reproducir automáticamente
+          onError={handleVideoError} // Detecta si hay un error al intentar reproducir el video
+        >
+          <source src={heroVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      ) : (
+        <img
+          src={heroImage}
+          alt="Hero Background"
+          className="absolute inset-0 object-cover w-full h-full"
+        />
+      )}
       {/* Content Overlay */}
       <div className="relative z-10 flex flex-col bg-black bg-opacity-30 items-center justify-center h-full text-center text-white px-4 sm:px-6 md:px-8 lg:px-12">
         {/* Wrapper for text content */}
@@ -30,7 +49,6 @@ export const HeroSection: React.FC = () => {
               Earn passive income, without the hefty price tag. Invest in rental properties, securely and easily.
             </p>
             <NewsletterForm />
-
           </div>
         </div>
       </div>
