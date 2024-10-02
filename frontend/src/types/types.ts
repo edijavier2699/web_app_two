61945693051d6f9ab1cac29594277b3ead260ddd
edijavier2699 <edison.canizares@tokunize.com>
2024-10-02 16:13:51 +0100

    types solved

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -3,13 +3,17 @@ export interface NavigationItem {
     href: string;
     current: boolean;
   }
+
+  export interface Question {
+    id: string;
+    title: string;
+    content: string;
+}
+
   
  export  interface FaqCategory {
     title: string;
     description: string;
     path: string;
     id:number,
-
   }
-
- 
