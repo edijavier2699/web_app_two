18b68b05de637964f3eb18007efb729aa4603659
edijavier2699 <edison.canizares@tokunize.com>
2024-10-23 11:10:34 +0100

    invitation request bug solved

--- backend/newsletter/urls.py
+++ backend/newsletter/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('api/subscribe/', views.DemoUserListView.as_view(), name="demo-list"),
     path('contacted-clients/', views.ContactedClientListCreateView.as_view(), name='contacted-client-list-create'),
     path('send-newsletter/', views.SendNewsletter.as_view(), name="send-newsletter"),
-    path('newsletter/', views.show_newsletter, name='mostrar_newsletter'),
+    path("api/invitation-request/", views.InvitationRequestListView.as_view(), name="invitation-request-create")
 ]
